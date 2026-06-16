eb7295180f4bde1f4b61f603500a458b2d084dd9
HafizEngineerMuhammadAbdullah <abdullahkhalid2k25@gmail.com>
2026-06-16 15:18:30 -0700

    Complete MySQL Indexes

--- SQL_Database/Indexes_in_sql.sql
+++ SQL_Database/Indexes_in_sql.sql
@@ -5,3 +5,5 @@ SHOW INDEXES FROM users;
 SELECT * FROM users;
 CREATE INDEX idx_gender ON users (gender);
 SELECT * FROM users WHERE email = 'sara.ahmed@example.com' AND gender = 'Female';
+-- Create a Multi-Column index
+CREATE INDEX idex_gender_salary ON users(gender,salary);
