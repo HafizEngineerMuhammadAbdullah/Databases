f06c41ac1ce60358903ce3604780243c1899a260
HafizEngineerMuhammadAbdullah <abdullahkhalid2k25@gmail.com>
2026-06-18 12:56:27 -0700

    SubQueries in MySQL Completed!

--- SQL_Database/subqueries_in_mysql.sql
+++ SQL_Database/subqueries_in_mysql.sql
@@ -1,1 +1,16 @@
 -- SubQueries in MySQL :-
+USE startersql;
+-- SELECT AVG(salary) AS avg_salary FROM users; 
+
+SELECT * FROM users WHERE salary > (SELECT AVG(salary) FROM users);
+SELECT * FROM users WHERE salary < (SELECT AVG(salary) FROM users);
+
+SELECT id,name,email,referred_by_id 
+FROM users
+WHERE referred_by_id IN (
+SELECT id FROM users WHERE salary > 200000
+);
+
+SELECT id , name , salary ,
+(SELECT AVG(salary) FROM users) AS average_employee_salary
+FROM users;
