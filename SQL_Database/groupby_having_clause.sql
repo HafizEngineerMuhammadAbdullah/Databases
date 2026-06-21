b6952f7348b42dcb7a7c5973b2c473b5f4cc33e9
HafizEngineerMuhammadAbdullah <abdullahkhalid2k25@gmail.com>
2026-06-21 11:25:52 -0700

    complete group & having clause

--- SQL_Database/groupby_having_clause.sql
+++ SQL_Database/groupby_having_clause.sql
@@ -5,7 +5,7 @@ SELECT  gender, AVG(salary) AS 'Average Salary' FROM users GROUP BY gender;
 SELECT  gender AS 'Gender', AVG(salary) AS 'Average Salary', COUNT(*) AS 'No of Gender' FROM users GROUP BY gender;
 SELECT  gender AS 'Gender', AVG(salary) AS 'Average Salary', COUNT(*) AS 'No of Gender'
 FROM users
-GROUP BY gender
+GROUP BY gender WITH ROLLUP
 HAVING AVG(salary) < 61000;
 SELECT  gender AS 'Gender', AVG(salary) AS 'Average Salary', COUNT(*) AS 'No of Gender'
 FROM users
