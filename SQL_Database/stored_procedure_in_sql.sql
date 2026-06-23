176f01c651873cfa21ab2d1349b82a9ac3e87c8a
HafizEngineerMuhammadAbdullah <abdullahkhalid2k25@gmail.com>
2026-06-23 11:43:04 -0700

    Working on Stored Procedure

--- SQL_Database/stored_procedure_in_sql.sql
+++ SQL_Database/stored_procedure_in_sql.sql
@@ -1,8 +1,21 @@
 -- Stored Procedures In MySQL :-
 USE startersql;
 SELECT * FROM users;
+-- DELIMITER $$
+-- CREATE PROCEDURE select_users()
+-- BEGIN
+--   SELECT * FROM users;
+-- END $$
+-- DELIMITER ;
+
 DELIMITER $$
-CREATE PROCEDURE select_users()
+CREATE PROCEDURE AddUsers(
+	IN p_name VARCHAR(100),
+	IN p_email VARCHAR(100),
+	IN p_gender ENUM('Male','Female', 'Other'),
+	IN p_dob DATE,
+	IN p_salary INT
+)
 BEGIN
   SELECT * FROM users;
 END $$
