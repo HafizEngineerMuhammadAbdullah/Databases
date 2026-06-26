74e271638676fba34ead4d958769a843d9193d66
HafizEngineerMuhammadAbdullah <abdullahkhalid2k25@gmail.com>
2026-06-26 11:24:23 -0700

    Learning Triggers in SQL

--- SQL_Database/triggers_in_sql.sql
+++ SQL_Database/triggers_in_sql.sql
@@ -6,3 +6,13 @@ CREATE TABLE user_log(
   name VARCHAR(100),
   created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+DELIMITER $$
+CREATE TRIGGER after_user_insert
+AFTER INSERT ON users
+FOR EACH ROW 
+BEGIN
+   INSERT INTO user_log(user_id,name)
+   VALUES (NEW.id,NEW.name);
+END $$
+DELIMITER ;
